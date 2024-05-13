8abde917f767892681b5e21a56cde1c42d0a206e
Calkwalk <calkwalk@163.com>
2024-05-13 20:19:28 +0800

    first commit

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,14 @@
+
+import React, { Suspense } from 'react'
+import Thankyou from './Thankyou'
+
+const ThanksPage = () => {
+  
+  return (
+    <Suspense>
+      <Thankyou />
+    </Suspense>
+  )
+}
+
+export default ThanksPage
